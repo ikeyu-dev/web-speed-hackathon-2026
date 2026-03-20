7c9250b81ad433b8a25ed69c65882292b5f0164c
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2026-03-20 14:36:51 +0900

    perf: プロフィール画像にloading=lazyを追加

--- application/client/src/components/post/PostItem.tsx
+++ application/client/src/components/post/PostItem.tsx
@@ -22,6 +22,7 @@ export const PostItem = ({ post }: Props) => {
             >
               <img
                 alt={post.user.profileImage.alt}
+                loading="lazy"
                 src={getProfileImagePath(post.user.profileImage.id)}
               />
             </Link>
